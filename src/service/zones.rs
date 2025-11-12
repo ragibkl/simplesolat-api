d3c4b99743a00f347c4597af1ccaef3e81d4278a
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2025-11-12 12:06:30 +0800

    update zones yaml

--- src/service/zones.rs
+++ src/service/zones.rs
@@ -2,17 +2,25 @@ use diesel::PgConnection;
 use serde::Deserialize;
 use std::fs;
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Zone {
-    zone_code: String,
+    code: String,
     state: String,
     location: String,
+    latitude: f64,
+    longitude: f64,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ZoneConfig {
+    zones: Vec<Zone>,
 }
 
 impl From<&Zone> for crate::models::zones::UpsertZone {
     fn from(value: &Zone) -> Self {
         Self {
-            zone_code: value.zone_code.to_string(),
+            zone_code: value.code.to_string(),
             state: value.state.to_string(),
             location: value.location.to_string(),
         }
@@ -21,9 +29,9 @@ impl From<&Zone> for crate::models::zones::UpsertZone {
 
 pub fn read_zones() -> Vec<Zone> {
     let file = fs::File::open("data/zones.yaml").unwrap();
-    let zones: Vec<Zone> = serde_yaml::from_reader(file).unwrap();
+    let zone_config: ZoneConfig = serde_yaml::from_reader(file).unwrap();
 
-    zones
+    zone_config.zones
 }
 
 pub fn upsert_zones_from_data(conn: &mut PgConnection) {
